b295378afb09c256b9cba223a3c175a65b78df2d
Simone Icardi <info@simoneicardi.com>
2018-02-25 16:27:56 +0100

    added some clarification comments

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -14,7 +14,7 @@ global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
-// build 'renderComponent' helper that should render a given react class
+// build 'renderComponent' helper that should render a given react class [later in a DOM]
 function renderComponent(ComponentClass, props, state) {
   const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
@@ -22,18 +22,18 @@ function renderComponent(ComponentClass, props, state) {
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML [in the specified DOM, in this case that one created by jsdom and warpped in jquery]
 }
 
 // Build helper for simulating events
-$.fn.simulate = function(eventName, value) {
+$.fn.simulate = function(eventName, value) { //$.fn. creates a new jquery method for our jquery instance
   if (value) {
-    this.val(value);
+    this.val(value);// this is the jquery obj and the jq method .val(value) sets the value passed, nothing new here
   }
-  TestUtils.Simulate[eventName](this[0]);
+  TestUtils.Simulate[eventName](this[0]); // refer to react-addons-test-utils doc to undurstend this [react doc - addons - testutil]
 }
 
 // Set up chai-jquery
-chaiJquery(chai, chai.util, $);
+chaiJquery(chai, chai.util, $); // chaijq set up function, see doc if you want
 
 export { renderComponent, expect };
