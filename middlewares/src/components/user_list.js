829c1275742c58ad0c578cbcb9b04f9198833aee
Simone Icardi <info@simoneicardi.com>
2018-03-06 19:46:33 +0100

    enanched some comments

--- middlewares/src/components/user_list.js
+++ middlewares/src/components/user_list.js
@@ -9,7 +9,7 @@ class UserList extends Component {
 
   renderUser(user) {
     return (
-      <div className="card card-block" key={user.id} >// added an id as a key
+      <div className="card card-block" key={user.id} > {/* added an id as a key*/}
         <h4 className="card-title">{user.name}</h4>
         <p className="card-text">{user.company.name}</p>
         <a className="btn btn-primary" href={user.website}>Website</a>
--- middlewares/src/middlewares/async.js
+++ middlewares/src/middlewares/async.js
@@ -5,7 +5,7 @@ export default function({ dispatch }) {
     // we dont care about it, send it on
     if (!action.payload || !action.payload.then) {
       return next(action);
-    }
+    } // This case hadle no just another potential action that we don't want to treat here, but also the same(new) action in the new 'state' [and that's why we don't check for the action.type]
 
     // Make sure the action's promise resolves
     action.payload
@@ -13,7 +13,7 @@ export default function({ dispatch }) {
         // create a new action with the old type, but
         // replace the promise with the reponse data
         const newAction = { ...action, payload: response };
-        dispatch(newAction);
+        dispatch(newAction); /* dispatch the new action to the top of the middleware stack (so the action will be evalueted again and do some other operation if/when needed [not this one the case now but you never know]) and finelly goes to reducers*/
       });
   }
 }
