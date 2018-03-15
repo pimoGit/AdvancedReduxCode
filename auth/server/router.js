5c22444bdfb0a26b070b3f02a92203a10ccaffa3
Simone Icardi <info@simoneicardi.com>
2018-03-15 19:21:14 +0100

    added useful comments

--- auth/server/router.js
+++ auth/server/router.js
@@ -2,11 +2,11 @@ const Authentication = require('./controllers/authentication');
 const passportService = require('./services/passport');
 const passport = require('passport');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
+const requireAuth = passport.authenticate('jwt', { session: false });// session: false means we don't want cookies set (we use jwt)
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/', requireAuth, function(req, res) {
+  app.get('/', requireAuth, function(req, res) { // we say to use the requireAuth (we create above) to handle this route req before sendig a res
     res.send({ message: 'Super secret code is ABC123' });
   });
   app.post('/signin', requireSignin, Authentication.signin);
--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -6,14 +6,14 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
 
 // Create local strategy
-const localOptions = { usernameField: 'email' };
+const localOptions = { usernameField: 'email' }; // just to set that if it serach for user has to loook at email
 const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
   // Verify this email and password, call done with the user
   // if it is the correct email and password
   // otherwise, call done with false
   User.findOne({ email: email }, function(err, user) {
-    if (err) { return done(err); }
-    if (!user) { return done(null, false); }
+    if (err) { return done(err); }// err in the operation
+    if (!user) { return done(null, false); } // no err but email (so user) not found
 
     // compare passwords - is `password` equal to user.password?
     user.comparePassword(password, function(err, isMatch) {
@@ -27,22 +27,22 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
 
 // Setup options for JWT Strategy
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.secret
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),//where to find the jtw (in this case header-authorization from the request)
+  secretOrKey: config.secret //what to use to decript the token (so we can have the user id back)
 };
 
 // Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   // See if the user ID in the payload exists in our database
-  // If it does, call 'done' with that other
+  // If it does, call 'done' with that user
   // otherwise, call done without a user object
-  User.findById(payload.sub, function(err, user) {
-    if (err) { return done(err, false); }
+  User.findById(payload.sub, function(err, user) {// payload is the obj created with jwt.encode attached to the token in the db 
+    if (err) { return done(err, false); } // if there is an err and we cannot find user (coz of the err)
 
     if (user) {
-      done(null, user);
+      done(null, user);// there is no err e we find a user
     } else {
-      done(null, false);
+      done(null, false);// there is no err but we don't find the user
     }
   });
 });
